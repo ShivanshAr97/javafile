67db86e7cc93a4499dc9ed34a12df9e8e81c344f
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2023-06-23 23:21:49 +0530

    commit

--- 2b(Searching,Sorting).java
+++ 2b(Searching,Sorting).java
@@ -1,18 +1,53 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String args[]) throws Exception
-	{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-		int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
 
-		int index = Arrays.binarySearch(array, 2);
+        int[] inputArray = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            inputArray[i] = scanner.nextInt();
+        }
 
-		if (index >= 0)
-			System.out.println("Element 2 found at index: "
-							+ index);
-		else
-			System.out.println("Element not found");
+        
+        System.out.print("Enter the element to be searched in the array: ");
+        int k = scanner.nextInt();
+
+		System.out.println(Find(inputArray,k));
+		
+		Sort(inputArray);
+		
+		for(int i=0;i<inputArray.length;i++){
+		    System.out.println(inputArray[i]);
+		}
+        
+        scanner.close();
+    }
+
+	public static int Find(int[] arr, int target){
+		for(int i=0;i<arr.length;i++){
+			if(arr[i]==target){
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	public static void Sort(int[] arr){
+	    int temp;
+	    for(int i=0;i<arr.length;i++){
+	        for(int j=i+1;j<arr.length;j++){
+	            if(arr[i]>arr[j]){
+    	            temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+	            }
+	        }
+	    }
 	}
 }
